7599c6a801601781336f54f02ce9cfd0238499a7
Jerrehy <sacha15012000@gmail.com>
2022-02-14 00:39:26 +0300

    Deploy

--- agency_app/config.py
+++ agency_app/config.py
@@ -1,10 +1,10 @@
 class Config:
     # Данные конфигурации подключения к БД
     dialect = 'postgresql'
-    username = 'postgres'
-    password = 'Qwerty7'
-    host = 'localhost'
-    db_name = 'employment_agency'
+    username = 'uiezfrusneudzn'
+    password = '9506ead55c0f930839988a21ffac242203b044a97e18f18f0b20e9f8c84c9f90'
+    host = 'ec2-34-249-49-9.eu-west-1.compute.amazonaws.com'
+    db_name = 'deau5afe7llnqc'
 
     # Настройки для экземляра: секретный ключ запуска и путь к БД
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
